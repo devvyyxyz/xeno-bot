a0ca04d100e26bcf7c2b25a114aaffff037e9651
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2026-03-11 20:33:58 -0500

    Format eslint.config.mjs with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,37 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended"),
+export default defineConfig([
+  {
+    extends: compat.extends('eslint:recommended'),
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
 
-        ecmaVersion: 12,
-        sourceType: "module",
+      ecmaVersion: 12,
+      sourceType: 'module',
     },
 
     rules: {
-        "no-console": "off",
-        "no-unused-vars": "off",
-        "no-useless-assignment": "off",
-        "no-useless-escape": "off"
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'no-useless-assignment': 'off',
+      'no-useless-escape': 'off',
     },
-}]);
+  },
+]);
